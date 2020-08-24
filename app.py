bec3bcc5f8d8fe31ecf6fcd75453cf6b629b58fd
SUYASH JAMDADE <60968407+suyash-jamdade@users.noreply.github.com>
2020-08-24 20:43:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
-from flask_sqlalchemy import SQLAlchemy
-import os 
 
 app = Flask(__name__)
 
